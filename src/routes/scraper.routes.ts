baf787732ec974cae222780fa799312075cae194
Tran Minh Dang <tranminhdang0702@gmail.com>
2026-03-03 00:20:55 +0700

    Feature/Axios_Cherrio_scraper_1_UpdateLogic

--- src/routes/scraper.routes.ts
+++ src/routes/scraper.routes.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import { scraperController } from "../controller";
+import validate from "../middleware/validate";
+import { scraperValidation } from "../validation";
 
 const router = express.Router();
 
-router.get("/", scraperController.getScrape);
+router.get(
+  "/",
+  validate(scraperValidation.axiosScraper),
+  scraperController.getScrape,
+);
 export default router;
