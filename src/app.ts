6e7544d8152fcca8e4de73129a6d3608c2a9d1a7
Tran Minh Dang <tranminhdang0702@gmail.com>
2026-03-07 15:04:01 +0700

    Feature/nodecron_hard_delete

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import router from "./routes";
+import { dayjsService } from "./service";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 //config req.body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+dayjsService.hardCleanUpUsers();
 app.use("/", router);
 app.listen(PORT, () => {
   console.log(`Running on ${PORT}`);
